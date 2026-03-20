514b27c7267bcc2d2d0bcc7407daa915fade88a4
Bhuvaneshvar <bhuvaneshvar8@gmail.com>
2026-03-20 22:57:00 +0530

    fixed db and vercel integration

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,10 @@ import app from './app';
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(Number(PORT), '0.0.0.0', () => {
-    console.log(`Server is running on http://0.0.0.0:${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production' || !process.env.VERCEL) {
+    app.listen(Number(PORT), '0.0.0.0', () => {
+        console.log(`Server is running on http://0.0.0.0:${PORT}`);
+    });
+}
+
+export default app;
